9b1d537c23060e7e151b0858e16863eddea0094b
Connor Grieb <congrieb@umich.edu>
2014-10-21 11:39:28 -0400

    created multithreaded scan_and_create method

--- yikbot.py
+++ yikbot.py
@@ -1,5 +1,15 @@
 import pyak
 import time
+from multiprocessing import Process
+
+def threaded_scan(pattern1, pattern2):
+	yakker = pyak.Yakker()
+	print "created a new yakker on a new thread"
+	print pattern1
+	print pattern2
+	time.sleep(10)
+	print "waking yakker back up"
+	#yakker.single_scan(pattern1, pattern2)
 
 class YikBot(pyak.Yakker):
     yakkers = []
@@ -15,7 +25,7 @@ class YikBot(pyak.Yakker):
             print "DEBUG: Going to sleep, will repeat in 10 seconds"
             time.sleep(10)
 
-    def scan(self, pattern1, pattern2):
+    def multi_scan(self, pattern1, pattern2):
         while True:
             print "DEBUG: Scanning All yaks for"
             print pattern
@@ -27,7 +37,21 @@ class YikBot(pyak.Yakker):
                         yak.upvote()
                 print "DEBUG: Sleeping for 2 seconds and scanning again"
                 time.sleep(2)
-                    
+
+    def single_scan(self, pattern1, pattern2):
+        while True:
+            print "DEBUG: Yakker "
+            print self
+            print "DEBUG: Scanning All yaks for"
+            print pattern1
+            print pattern2
+	    yaks = self.get_yaks()
+            for yak in yaks:	
+                if yak.message.find(pattern1) != -1 or yak.message.find(pattern2) != -1:
+                    print "Found Yak containing target pattern"
+                    yak.upvote()
+                print "DEBUG: Sleeping for 2 seconds and scanning again"
+                time.sleep(2)                   
 
     def respond(self, yak):
         yak.add_comment("Hi, I'm yikBot")
@@ -81,6 +105,11 @@ class YikBot(pyak.Yakker):
             print "DEBUG: Registered new user with id %s" % yakker.id
             yakker.update_location(self.location)
             self.yakkers.append(yakker)
+
+    def create_and_scan(self, count, pattern1, pattern2):
+        for i in range(0, count):
+            p = Process(target = threaded_scan, args = (pattern1, pattern2))
+            p.start()
 
     def clear_yakkers(self):
         self.yakkers = []
